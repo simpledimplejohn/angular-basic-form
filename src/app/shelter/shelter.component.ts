d8fe7781ff60601d00bb53a9475862f4872883f4
Simple Dimple John <simpledimplejohn@gmail.com>
2022-03-17 15:19:59 -0700

    complete shelter component--throwing error on event

--- src/app/shelter/shelter.component.ts
+++ src/app/shelter/shelter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-shelter',
@@ -7,9 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShelterComponent implements OnInit {
   locations: string[] = ['Downtown', 'S. County', 'Lakeside'];
-  constructor() { }
+  volunteerForm: FormGroup;
+
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  initializeForm(): void {
+    this.volunteerForm = this.fb.group({
+      name: 'Name Here',
+      phoneNumber: '',
+      preferredLocation: '',
+      animals: this.fb.group({
+        dogs: false,
+        cats: false,
+        reptiles: false
+      }),
+      references: this.fb.array([
+        this.fb.control('')
+      ])
+    })
+  }
+  onSubmit(): void {
+    console.log(this.volunteerForm);
+  }
+
+  addEmail(): void {
+    this.references.push(this.fb.control(''));
+  }
+
+  removeEmail(index: number): void {
+    this.references.removeAt(index);
+  }
+
+  // selectLocation(event: { target: { value: any; }; }): void {
+  //   this.volunteerForm.patchValue({
+  //     preferredLocation: event.target.value
+  //   });
+  // }
+
+
+
+  // getter, not sure what this is?
+  get references(): FormArray {
+    return this.volunteerForm.get('references') as FormArray;
   }
 
 }
